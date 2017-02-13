a30bb01b039b1d8ea289cfdc1343de625ce9c021
Parto Karwat <partokarwat@gmail.com>
2017-02-13 18:14:58 +0100

    feat: Add spark graph to stock details view

--- app/src/main/java/com/udacity/stockhawk/ui/DetailFragment.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailFragment.java
@@ -2,6 +2,7 @@ package com.udacity.stockhawk.ui;
 
 
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,8 +14,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.EntryXComparator;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract.Quote;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A fragment containing the history of a stock.
@@ -46,6 +64,7 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
     public static final int COL_QUOTE_HISTORY = 5;
 
     private TextView testView;
+    private LineChart mChart;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -63,6 +82,26 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
 
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         testView = (TextView) rootView.findViewById(R.id.symbol);
+
+        mChart = (LineChart) rootView.findViewById(R.id.sparkView);
+
+        // no description text
+        mChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        mChart.setDragDecelerationFrictionCoef(0.9f);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+        mChart.setHighlightPerDragEnabled(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+        mChart.setViewPortOffsets(0f, 0f, 0f, 0f);
 
         return rootView;
     }
@@ -95,6 +134,91 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
         if (data != null && data.moveToFirst()) {
             String symbol = data.getString(COL_QUOTE_SYMBOL);
             testView.setText(symbol);
+
+            String history = data.getString(COL_QUOTE_HISTORY);
+            String[] points = history.split("\n");
+
+            ArrayList<Entry> values = new ArrayList<>();
+
+            for (String point : points) {
+                String[] axis = point.split(",");
+                float x = TimeUnit.MILLISECONDS.toHours(Long.valueOf(axis[0]));
+                float y = Float.valueOf(axis[1]);
+                values.add(new Entry(x, y));
+            }
+
+            // always sort entries, unsorted nothing will be drawn!
+            Collections.sort(values, new EntryXComparator());
+
+            // create a dataset and give it a type
+            LineDataSet set1 = new LineDataSet(values, getString(R.string.spark_line_label));
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setValueTextColor(ColorTemplate.getHoloBlue());
+            set1.setLineWidth(1.5f);
+            set1.setDrawCircles(false);
+            set1.setDrawValues(false);
+            set1.setFillAlpha(65);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setDrawCircleHole(false);
+            set1.setDrawFilled(true);
+
+            // create a data object with the datasets
+            LineData chartData = new LineData(set1);
+            chartData.setValueTextColor(Color.RED);
+            chartData.setValueTextSize(9f);
+
+            // set data
+            mChart.setData(chartData);
+
+            // dont forget to refresh the drawing
+            mChart.invalidate();
+
+            // get the legend (only possible after setting data)
+            Legend l = mChart.getLegend();
+            l.setEnabled(false);
+
+            XAxis xAxis = mChart.getXAxis();
+            xAxis.enableGridDashedLine(10f, 10f, 0f);
+            xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+            xAxis.setTextSize(10f);
+            xAxis.setTextColor(Color.WHITE);
+            xAxis.setDrawAxisLine(false);
+            xAxis.setDrawGridLines(true);
+            xAxis.setTextColor(getResources().getColor(R.color.textColorChartAxis));
+            xAxis.setCenterAxisLabels(true);
+            xAxis.setGranularity(1f); // one hour
+            xAxis.setAxisMaximum(Collections.max(values, new EntryXComparator()).getX() + 2000);
+            xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+                private SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM yy");
+
+                @Override
+                public String getFormattedValue(float value, AxisBase axis) {
+
+                    long millis = TimeUnit.HOURS.toMillis((long) value);
+                    return mFormat.format(new Date(millis));
+                }
+            });
+
+            YAxis leftAxis = mChart.getAxisLeft();
+            leftAxis.enableGridDashedLine(10f, 10f, 0f);
+            leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+            leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+            leftAxis.setDrawGridLines(true);
+            leftAxis.setGranularityEnabled(true);
+            leftAxis.setAxisMinimum(0f);
+            leftAxis.setAxisMaximum(Collections.max(values, new EntryXComparator()).getY() + Collections.min(values, new EntryXComparator()).getY());
+            leftAxis.setYOffset(-9f);
+            leftAxis.setTextColor(getResources().getColor(R.color.textColorChartAxis));
+
+            // limit lines are drawn behind data (and not on top)
+            leftAxis.setDrawLimitLinesBehindData(true);
+
+            YAxis rightAxis = mChart.getAxisRight();
+            rightAxis.setEnabled(false);
+
         }
     }
 
